ae33f5d074bc8aa04f7c8bc73003c7e9014a80ce
IndiraMatlori <indiramatlori653@gmail.com>
2025-07-07 20:24:35 +0530

    First Commit

--- src/main.java
+++ src/main.java
@@ -1,15 +1,6 @@
 public class main {
     public static void main(String[] args) {
-
-        DownloadThread d1= new DownloadThread("http://amazon.com//");
-        DownloadThread d2= new DownloadThread("http://flipcart.com//");
-        DownloadThread d3= new DownloadThread("http://mintra.com//");
-        DownloadThread d4= new DownloadThread("http://messho.com//");
-
-        d1.start();
-        d2.start();
-        d3.start();
-        d4.start();
-        d1.threadId();
+    String sourcePath="D:\\Java\\File\\src\\Source File";
+    String copyPath="D:\\Java\\File\\src\\copyFile";
     }
 }
