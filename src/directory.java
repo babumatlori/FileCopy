b08962e4f12c6b40ac73d6da967da183ba8d9e48
babumatlori <babumatlori@gmail.com>
2025-07-08 01:09:50 +0530

    Update directory.java

--- src/directory.java
+++ src/directory.java
@@ -4,28 +4,32 @@ public class DirectoryCopy {
 
     public static void copy(File src, File dest) throws IOException {
         if (src.isFile()) {
-            // If src is a file, copy the content
+           
+
             copyFile(src, new File(dest, src.getName()));
             return;
         }
 
-        // If src is a directory, create a new directory in dest
+        
+
         File newDir = new File(dest, src.getName());
         if (!newDir.exists()) {
             newDir.mkdir();
         }
 
-        // Get all files/subdirectories
+       
+
         File[] files = src.listFiles();
         if (files != null) {
             for (File file : files) {
-                // Recursive call
+             
                 copy(file, newDir);
             }
         }
     }
 
-    // Helper method to copy file content
+    
+
     public static void copyFile(File sourceFile, File destFile) throws IOException {
         try (
             FileInputStream fis = new FileInputStream(sourceFile);
